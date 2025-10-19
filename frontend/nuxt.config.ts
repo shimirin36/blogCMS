3fedd0fa4ea11492685852cee242c58f5c99a90e
shimirin36 <ba07bo30lat@icloud.com>
2025-10-19 13:47:38 +0900

    ymlファイルのlaravel.testをlaravelに変更

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      apiBase: 'http://laravel.test/api'
+      apiBase: 'http://laravel/api'
     }
   }
 })
